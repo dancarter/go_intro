a9eb36d34495d1a0c7208de67aef80f1fd3bd69f
Dan Carter <dcarter@adharmonics.com>
2014-12-28 01:22:08 -0500

    Update to greet array of names

--- intro_to_go.go
+++ intro_to_go.go
@@ -5,8 +5,23 @@ import (
 )
 
 func main() {
-  // var name string = "Daniel"
-  name := "Daniel"
+  // Long form variable declaration  - var name string = "Daniel"
+  // Short form variable declaration - name := "Daniel"
 
-  fmt.Println("Hello " + name)
+  names := []string {
+    "Daniel",
+    "Mark",
+    "Rachel",
+    "Dylan",
+    "Leo",
+  }
+
+  // _     - index, not being used so _ throws it away.
+  //         Can be set to a variable to be used if needed
+  // n     - the value at the current index in the array
+  // range - takes the array names and passes back each
+  //         value with its index
+  for _, n := range names {
+    fmt.Println("Hello, " + name)
+  }
 }
