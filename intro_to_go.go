da4969dbd983030236d2384aedb525a29ba3b330
Dan Carter <dcarter@adharmonics.com>
2014-12-28 01:23:55 -0500

    Add Greet function

--- intro_to_go.go
+++ intro_to_go.go
@@ -4,6 +4,14 @@ import (
   "fmt"
 )
 
+// capitalizing the first letter of the function makes it
+// available outside if this where to be imported. Basically:
+// lowercase = private function
+// uppercase = public function
+func Greet(name string) {
+  fmt.Println("Hello, " + name)
+}
+
 func main() {
   // Long form variable declaration  - var name string = "Daniel"
   // Short form variable declaration - name := "Daniel"
@@ -22,6 +30,6 @@ func main() {
   // range - takes the array names and passes back each
   //         value with its index
   for _, n := range names {
-    fmt.Println("Hello, " + name)
+    Greet(n)
   }
 }
