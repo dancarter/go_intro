ca98749c7d06e9e585bcde5f33524265d8cb0592
Dan Carter <dcarter@adharmonics.com>
2014-12-29 19:03:05 -0500

    Add buffered channel example

---
+++ channel-buffering.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// By default, channels are 'unbuffered', meaning they will
+// only accept sends (chan <-) if there is a corresponding
+// receive (<- chan) ready to receive the sent value.
+// 'Buffered channels' accept a limited number of values
+// without a corresponding receiver for those values
+
+func main() {
+  // Making a channel of strings buffering up to 2 values
+  messages := make(chan string, 2)
+
+  // Because the channel is buffered, we can send these
+  // values into the channel without a corresponding
+  // concurrent receiver
+  messages <- "buffered"
+  messages <- "channel"
+
+  // Later, we can receive the values as usual
+  fmt.Println(<-messages)
+  fmt.Println(<-messages)
+}
